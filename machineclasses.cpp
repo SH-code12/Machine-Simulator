fba48f3e953d26f3fd6d193d74a51ce7e2b41f2a
Hana <112332153+eng-hana74@users.noreply.github.com>
2023-11-15 19:55:16 +0200

    Update machineclasses.cpp

--- machineclasses.cpp
+++ machineclasses.cpp
@@ -18,7 +18,7 @@ string decemalToBase(int val,int base){
 }
 int baseToDecemal(string val, int base){
     reverse(val.begin(),val.end());
-    int pos=0, num;
+    int pos=0, num=0;
     for (size_t i = 0; i < val.size(); i++) {
         char &x = val[i];
         int d = (isalpha(x) ? x - 'A' + 10 : x - '0');
@@ -33,3 +33,51 @@ int baseToDecemal(char x){
     int d = (isalpha(x) ? x - 'A' + 10 : x - '0');
     return d;
 }
+memory::memory(int v){
+    value=v;
+}
+void memory:: set(int v){
+    value=v;
+}
+int memory::get()const{
+    return value;
+}
+string memory::binaryValue()const{
+    string biValue= decemalToBase(value,2);
+    while (biValue.size()<8){
+        biValue='0'+biValue;
+    }
+    return biValue;
+    
+}
+string memory::hexaValue()const{
+    string hexValue= decemalToBase(value,16);
+    while (hexValue.size()<2){
+        hexValue='0'+hexValue;
+    }
+    return hexValue;
+}
+int memory::twosComplementValue() const{
+    string biValue=binaryValue();
+    int num;
+    bool rev=0;
+    if (biValue[0]=='1'){
+        int size=biValue.size();
+        for (int i = size-1; i >=0; i--)
+        {
+            if (rev) {
+            // If we are in the process of taking two's complement, invert the bit
+            biValue[i] = !(biValue[i] - '0') + '0';
+
+            // If the current bit is '1', set rev to true
+            if (biValue[i] - '0') {
+                rev = true;
+            }
+        }
+    }
+    }
+
+    // Convert the inverted binary string to decimal and multiply by -1
+    return baseToDecemal(biValue, 2) * -1;
+        
+    }
